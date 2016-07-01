5d8fcf109e0b2e56c05f22721dc2b1130babe365
jellyfishgh <1129088994@qq.com>
2016-07-02 01:02:29 +0800

    mac:fetch data and rending

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 var util = require('./util');
 
-var search = util.parse(window.location.search.substr(1));
+var search = util.parse(location.search.substr(1));
 
-var platform = search.platform,
-    version = util.v2n(search.version);
+var platform = search.platform ? search.platform : util.whichPlatform(navigator.userAgent),
+    version = search.version;
 
-util.fetch('./' + platform + '.json', function(history) {
-    
-}, function() {
+util.fetch('./' + platform + '.json', function (vs) {
+    if (!version || util.v2n(version) < util.v2n(vs[0].version)) {
+        util.find('banner').style.display = 'block';
+        util.find('download').href = util.getDownloadUrl(platform);
+        util.find('bs3').ontouchend = function (e) {
+            e.stopPropagation();
+            banner.style.display = "none";
+        }
+    }
+    var container = util.find('container');
+    vs.map(function (v) {
+        container.appendChild(util.createVersionView(v));
+    });
+}, function () {
     util.find('container').innerHTML = "<div class='center fail'>加载失败，请稍后重试。</div>";
-}, function() {
+}, function () {
     util.find('centerDiv').style.display = 'none';
 });
--- src/util.js
+++ src/util.js
@@ -28,10 +28,18 @@ function parse(search) {
     return searchJson;
 }
 
+function whichPlatform(agent) {
+    if (/iPod|iPad|iPhone/ig.test(agent)) {
+        return 'iOS';
+    } else if (/Android/gi.test(agent)) {
+        return 'Android';
+    }
+}
+
 function fetch(url, resolve, reject, complete) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
             if (this.status == 200) {
                 resolve(JSON.parse(this.responseText));
@@ -44,10 +52,51 @@ function fetch(url, resolve, reject, complete) {
     xhr.send();
 }
 
+function getDownloadUrl(platform) {
+    if (platform === 'iOS') {
+        return '';
+    } else if (platform === 'Android') {
+        return '';
+    }
+}
+
+function createTitleView(version, date) {
+    var titleView = create("div", "version", version);
+    titleView.appendChild(create("span", "date", date));
+    return titleView;
+}
+
+function createContentView(content) {
+    var contentView = create("div", "descView");
+    contentView.appendChild(create("div", "desc", content.name));
+    contentView.appendChild(createItemsView(content.items));
+    return contentsView;
+}
+
+function createItemsView(items) {
+    var olView = create("ol", "content");
+    items.map(function (item) {
+        olView.appendChild(create("li", "item", item));
+    });
+    return olView;
+}
+
+function createVersionView(v) {
+    var versionView = create("div", "v");
+    versionView.appendChild(createTitleView(v.version, v.date));
+    v.contents.map(function (content) {
+        versionView.appendChild(createContentView(content));
+    });
+    return versionView;
+}
+
 module.exports = {
     v2n: v2n,
     create: create,
     find: find,
     parse: parse,
-    fetch: fetch
+    whichPlatform: whichPlatform,
+    fetch: fetch,
+    getDownloadUrl,
+    createVersionView: createVersionView
 };
